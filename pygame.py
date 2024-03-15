3608d200d609bc2822db60001bcce9a9c671bf26
Celyne <dianakydd2@gmail.com>
2024-03-15 15:29:46 +0300

    added comments

--- pygame.py
+++ pygame.py
@@ -1,17 +1,21 @@
 import pygame
+# line 1: import pygame and let it serve us
 
-run = True
-width = 400
-height = 100
-pygame.init()
-screen = pygame.display.set_mode((width, height))
-font = pygame.font.SysFont(None, 48)
-text = font.render("Welcome to pygame", True, (255, 255, 255))
+run = True #the program will run as long as the run variable is True
+width = 400 #determine the window's size
+height = 100 #determine the window's size
+pygame.init() #initialize the pygame environment
+screen = pygame.display.set_mode((width, height)) #prepare the application window and set its size
+font = pygame.font.SysFont(None, 48) #make an object representing the default font of size 48 points
+text = font.render("Welcome to pygame", True, (255, 255, 255)) 
+  #make an object representing a given text – the text will be anti-aliased (True) and white (255,255,255)
 screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
-pygame.display.flip()
-while run:
-  for event in pygame.event.get():
+  #insert the text into the (currently invisible) screen buffer
+pygame.display.flip() #flip the screen buffers to make the text visible
+while run: #the pygame main loop starts here
+  for event in pygame.event.get(): #get a list of all pending pygame events
    if event.type == pygame.QUIT\
    or event.type == pygame.MOUSEBUTTONUP\
    or event.type == pygame.KEYUP:
-    run = False
+    #lines 17 through 19: check whether the user has closed the window or clicked somewhere inside it or pressed any key
+    run = False #if yes, stop executing the code.
